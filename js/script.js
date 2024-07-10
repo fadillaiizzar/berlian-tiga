c188baf83df034a7f5a70aeef10d7c4f773a5fe1
fadillaiizzar <fadillaiizzarm@gmail.com>
2024-07-10 18:04:03 +0700

    first up

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const iconNav = document.getElementById("icon-nav");
+const menuList = document.getElementById("menu-list");
+iconNav.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+});
+
+
+
+// const menuBar = document.querySelector(".box-menu-bar");
+// const menuNav = document.querySelector(".box-navbar");
+
+// menuBar.addEventListener('click' , function(){
+//     menuNav.classList.toggle("menu-active");
+// });
